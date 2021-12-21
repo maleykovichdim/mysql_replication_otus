bbaf73e0bccab1b14f47e6a4bd4cc59392818041
dmitry maleykovich <dmitry.maleykovich@opendesign.com>
2021-12-21 22:07:35 +0200

    add dockprom

---
+++ dockprom/run.bat
@@ -0,0 +1,4 @@
+set ADMIN_USER=admin
+set ADMIN_PASSWORD=admin
+set ADMIN_PASSWORD_HASH=JDJhJDE0JE91S1FrN0Z0VEsyWmhrQVpON1VzdHVLSDkyWHdsN0xNbEZYdnNIZm1pb2d1blg4Y09mL0ZP
+docker-compose up --build -d
